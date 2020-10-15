f55faa34bbcc9735098dc4fbd1ce8e1dd8a798be
Daniel <DanP1925@gmail.com>
2020-10-15 18:49:02 -0500

    jugando con el bot

--- slack-expobot/bot.rb
+++ slack-expobot/bot.rb
@@ -1,4 +1,11 @@
 module SlackExpobot
     class Bot < SlackRubyBot::Bot
+        class Weather < SlackRubyBot::Bot
+            match /Que hora es\?/ do |client, data, match|
+                time = Time.new
+                hora = time.strftime("%k:%M")  
+                client.say(channel: data.channel, text: "La hora es #{hora}")
+            end
+          end
     end
 end
